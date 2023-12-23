bc37db98947002fc5b61e906c917d705bc5c8525
platocrat <jack.abe.winfield@gmail.com>
2023-12-22 23:55:00 -0600

    fix: API routes for sign in and sign up

--- src/app/api/sign-in/route.ts
+++ src/app/api/sign-in/route.ts
@@ -8,40 +8,40 @@ export async function POST(
   req: NextRequest,
   res: NextResponse,
 ) {
-  // if (req.method === 'POST') {
-  //   const { email, password } = req.body as any
+  if (req.method === 'POST') {
+    const { email, password } = await req.json()
 
-  //   const input = {
-  //     TableName: process.env.NEXT_BESSI_SIGN_IN_TABLE_NAME,
-  //     Key: { email },
-  //   }
+    const input = {
+      TableName: process.env.NEXT_BESSI_SIGN_IN_TABLE_NAME,
+      Key: { email },
+    }
 
-  //   const command = new GetCommand(input)
+    const command = new GetCommand(input)
 
-  //   try {
-  //     const { Item } = await ddbDocClient.send(command)
+    try {
+      const { Item } = await ddbDocClient.send(command)
 
-  //     if (Item && Item.password === password) {
-  //       return NextResponse.json(
-  //         { message: 'User has successfully signed in!' },
-  //         { status: 200 },
-  //       )
-  //     } else {
-  //       return NextResponse.json(
-  //         { error: 'Email and password do not match any known users.' },
-  //         { status: 400 },
-  //       )
-  //     }
-  //   } catch (error: any) {
-  //     return NextResponse.json(
-  //       { error: error.message },
-  //       { status: 500 },
-  //     )
-  //   }
-  // } else {
-  //   return NextResponse.json(
-  //     {},
-  //     { status: 405 },
-  //   )
-  // }
+      if (Item && Item.password === password) {
+        return NextResponse.json(
+          { message: 'User has successfully signed in!' },
+          { status: 200 },
+        )
+      } else {
+        return NextResponse.json(
+          { error: 'Email and password do not match any known users.' },
+          { status: 400 },
+        )
+      }
+    } catch (error: any) {
+      return NextResponse.json(
+        { error: error.message },
+        { status: 500 },
+      )
+    }
+  } else {
+    return NextResponse.json(
+      { error: 'Method Not Allowed' },
+      { status: 405 },
+    )
+  }
 }
