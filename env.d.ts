6fa542f0ff96eaa00185e927bc0c9bd82ed01f3e
platocrat <jack.abe.winfield@gmail.com>
2023-12-25 01:39:08 -0600

    remove unused env vars

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,5 @@
 namespace NodeJS {
   interface ProcessEnv {
-    NEXT_AWS_ROLE_ARN: string
     NEXT_BESSI_RESULTS_TABLE_NAME: string
     NEXT_BESSI_ACCOUNTS_TABLE_NAME: string
   }
--- src/utils/aws/dynamodb/index.ts
+++ src/utils/aws/dynamodb/index.ts
@@ -2,16 +2,10 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
 // Locals
-import { REGION, getTemporaryCredentials } from '..'
+import { REGION } from '..'
 
 const HOURS = 2
-const ROLE_SESSION_NAME = 'DynamoDB'
 
-const tempCredentials = await getTemporaryCredentials(HOURS, ROLE_SESSION_NAME)
-
-const ddbClient = new DynamoDBClient({
-  region: REGION,
-  credentials: tempCredentials,
-})
+const ddbClient = new DynamoDBClient({ region: REGION })
 
 export const ddbDocClient = DynamoDBDocumentClient.from(ddbClient)
