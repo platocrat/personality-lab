6caeaeec30237d990a7689f890eff65f3b4aa190
platocrat <>
2024-01-17 18:58:37 -0600

    fix: switch from Get to Query

--- src/app/api/check-email/route.ts
+++ src/app/api/check-email/route.ts
@@ -1,31 +1,37 @@
 // Externals
-import { GetCommand } from '@aws-sdk/lib-dynamodb'
 import { NextRequest, NextResponse } from 'next/server'
+import { QueryCommand, QueryCommandInput } from '@aws-sdk/lib-dynamodb'
 // Locals
 import { ddbDocClient } from '@/utils/aws/dynamodb'
 import { BESSI_ACCOUNTS_TABLE_NAME } from '@/utils'
 
 
+type BESSI_accounts = {
+  password: string
+  username: string
+  email: string
+  timestamp: number
+}
+
+
 export async function POST(req: NextRequest, res: NextResponse) {
   if (req.method === 'POST') {
     const { email } = await req.json()
-    
-    const timestamp = new Date().getTime()
 
-    const input = {
+    const input: QueryCommandInput = {
       TableName: BESSI_ACCOUNTS_TABLE_NAME,
-      Key: { 
-        email: email,
-        timestamp: timestamp
-      },
+      KeyConditionExpression: 'email = :emailValue',
+      ExpressionAttributeValues: {
+        ':emailValue': email,
+      }
     }
 
-    const command = new GetCommand(input)
+    const command = new QueryCommand(input)
 
     try {
       const response = await ddbDocClient.send(command)
 
-      if (response.Item !== undefined) {
+      if (response.Items && (response.Items[0] as BESSI_accounts).email) {
         return NextResponse.json(
           { message: 'Email exists!' },
           { status: 200 },
