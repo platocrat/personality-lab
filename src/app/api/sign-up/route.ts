cbc58524728ac1264fa77e6a57468e61330c73ad
platocrat <>
2024-01-18 18:36:55 -0600

    fix: query for username before signing up

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,23 +1,60 @@
 // Externals
-import { PutCommand } from '@aws-sdk/lib-dynamodb'
+import { 
+  PutCommand, 
+  QueryCommand, 
+  PutCommandInput, 
+  QueryCommandInput 
+} from '@aws-sdk/lib-dynamodb'
 import { NextRequest, NextResponse } from 'next/server'
 // Locals
 import { ddbDocClient } from '@/utils/aws/dynamodb'
 import { BESSI_ACCOUNTS_TABLE_NAME } from '@/utils'
+import { BESSI_accounts } from '../check-email/route'
 
 
 export async function POST(
   req: NextRequest,
   res: NextResponse,
-) {
+): Promise<NextResponse<{ message: string }> | NextResponse<{ error: any }>> {
   if (req.method === 'POST') {
     const { email, username, password } = await req.json()
 
-    console.log(`stored password: `, password)
+    let input: QueryCommandInput | PutCommandInput = {
+      TableName: BESSI_ACCOUNTS_TABLE_NAME,
+      IndexName: 'UsernameIndex',
+      KeyConditionExpression: 'username = :usernameValue',
+      ExpressionAttributeValues: {
+        ':usernameValue': username,
+      }
+    }
 
+    let command: QueryCommand | PutCommand = new QueryCommand(input)
+
+    try {
+      const response = await ddbDocClient.send(command)
+
+      console.log(`response: `, response)
+
+      if (response.Items && response.Items.length > 0) {
+        // Only return a response if the username exists in the DynamoDB Table
+        if ((response.Items[0] as BESSI_accounts).username) {
+          return NextResponse.json(
+            { message: 'Username exists' },
+            { status: 200 },
+          )
+        }
+      }
+    } catch (error: any) {
+      return NextResponse.json(
+        { error: error },
+        { status: 500 },
+      )
+    }
+    
+    // Get timestamp after the username is validated.
     const timestamp = new Date().getTime()
 
-    const input = {
+    input = {
       TableName: BESSI_ACCOUNTS_TABLE_NAME,
       Item: { 
         email: email,
@@ -27,7 +64,7 @@ export async function POST(
       },
     }
 
-    const command = new PutCommand(input)
+    command = new PutCommand(input)
 
     try {
       const response = await ddbDocClient.send(command)
