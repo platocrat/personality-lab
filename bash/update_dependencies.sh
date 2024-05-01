dc23b1c6bb1b3f9b81b402a47b76bf59a92b715d
jlmaldo2 <jlmaldo2@illinois.edu>
2024-05-01 12:23:35 -0500

    update: package dependencies

--- bash/update_dependencies.sh
+++ bash/update_dependencies.sh
@@ -1,42 +1,50 @@
 #!/bin/bash
 
-# Define the path to your project
-PROJECT_DIR="./"
+# Path to package.json
+PACKAGE_JSON="package.json"
 
-# Change to the project directory
-cd "$PROJECT_DIR"
-
-# Fetch outdated packages and format the output using awk
-npm outdated --json | awk -F'[:,]' '
-  /"latest"/ {
-    getline; # This gets the line with the version number
-    gsub(/[^0-9a-zA-Z\.-]/, "", $2); # Clean up the version string
-    print depName "@" $2; # Print the dependency@version
-  }
-  /"current"/ {
-    getline; # Move to the next line to get the dependency name
-    gsub(/"|:|,/, "", $2); # Clean up the package name
-    depName = $2; # Store the package name
-  }
-' | while read package; do
-    npm install "$package" --save
-done
-
-# Optionally, handle version control
-git add ../package.json
-git commit -m "update: node package dependencies"
-
-# Run the git command below to safely `--force`` because it ensures that you do
-# not overwrite any work on the remote branch that you haven't yet integrated 
-# into your local branch. It checks if the remote branch has updated since your 
-# last fetch, and if it has, the force push will be aborted.
-# ```zsh
-# git push origin <branch-name> --force-with-lease
-# ```
-
-# Check if a git command argument is provided
-if [ "$1" ]; then
-    eval "$1"
-else
-    git push
+# Check if package.json exists
+if [ ! -f "$PACKAGE_JSON" ]; then
+    echo "package.json not found"
+    exit 1
 fi
+
+# Function to update packages
+update_packages () {
+    # Using sed to extract lines from one pattern to another
+    sed -n "/$1/,/}/p" $PACKAGE_JSON | grep '": ".*"' | while read -r line
+    do
+        # Extract the package name
+        package_name=$(echo $line | cut -d '"' -f 2)
+        echo "Updating $package_name to the latest version..."
+        npm install "$package_name"@latest
+    done
+}
+
+# Update dependencies and devDependencies
+echo "Updating dependencies..."
+update_packages "dependencies"
+
+echo "Updating devDependencies..."
+update_packages "devDependencies"
+
+echo "All packages have been updated."
+
+# # Optionally, handle version control
+# git add package.json
+# git commit -m "update: node package dependencies"
+
+# # Run the git command below to safely `--force`` because it ensures that you do
+# # not overwrite any work on the remote branch that you haven't yet integrated 
+# # into your local branch. It checks if the remote branch has updated since your 
+# # last fetch, and if it has, the force push will be aborted.
+# # ```zsh
+# # git push origin <branch-name> --force-with-lease
+# # ```
+
+# # Check if a git command argument is provided
+# if [ "$1" ]; then
+#     eval "$1"
+# else
+#     git push
+# fi
