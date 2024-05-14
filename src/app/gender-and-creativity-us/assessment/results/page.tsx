62bb0a466b924d7d6141c51dbe1420b56b8ec224
platocrat <jack.abe.winfield@gmail.com>
2024-05-14 12:53:51 -0500

    fix: recursive import

--- src/app/gender-and-creativity-us/assessment/results/page.tsx
+++ src/app/gender-and-creativity-us/assessment/results/page.tsx
@@ -3,18 +3,18 @@ import { FC } from 'react'
 
 // Locals
 // Sections
-import Results from '@/sections/assessments/gender-and-creativity-us/results'
+import GenderAndCreativityUsResults from '@/sections/assessments/gender-and-creativity-us/results'
 // CSS
 import styles from '@/app/page.module.css'
 
 
 
-type GenderAndCreativityUsResultsProps = {}
+type ResultsProps = {}
 
 
 
 
-const GenderAndCreativityUsResults: FC<GenderAndCreativityUsResultsProps> = ({
+const Results: FC<ResultsProps> = ({
 
 }) => {
   return (
