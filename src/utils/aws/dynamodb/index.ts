a8738121634e82d9cd400dcfbe0d70a256c496e9
platocrat <jack.abe.winfield@gmail.com>
2024-05-22 19:43:14 -0500

    fix: because VSCode is stupid

---
+++ src/utils/aws/dynamodb/index.ts
@@ -0,0 +1,13 @@
+// Externals
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
+// Locals
+import { 
+  REGION,
+} from '..'
+
+const HOURS = 2
+
+const ddbClient = new DynamoDBClient({ region: REGION })
+
+export const ddbDocClient = DynamoDBDocumentClient.from(ddbClient)
---
+++ src/utils/aws/systems-manager/index.ts
@@ -0,0 +1,52 @@
+// Externals
+import { 
+  SSMClient,
+  GetParameterCommand, 
+  GetParameterCommandInput, 
+} from '@aws-sdk/client-ssm'
+import { NextResponse } from 'next/server'
+// Locals
+import { 
+  REGION,
+  AWS_PARAMETER_NAMES, 
+} from '..'
+
+
+
+export const ssmClient = new SSMClient({ region: REGION })
+
+/**
+ * @dev Fetches the requested parameter from AWS Parameter Store.
+ * @notice Assumes that the parameter is encrypted.
+ */
+export async function fetchAwsParameter(
+  parameterName: string
+): Promise<string | NextResponse<{ error: string }>> {
+  let parameter = 'null'
+
+  const input: GetParameterCommandInput = {
+    Name: parameterName,
+    WithDecryption: true,
+  }
+
+  const command = new GetParameterCommand(input)
+
+  try {
+    const response = await ssmClient.send(command)
+
+    if (response.Parameter?.Value) {
+      return parameter = response.Parameter?.Value
+    } else {
+      return NextResponse.json(
+        { error: `${ parameterName } parameter does not exist` },
+        { status: 400 }
+      )
+    }
+  } catch (error: any) {
+    // Something went wrong
+    return NextResponse.json(
+      { error: `Error! Something went wrong fetching ${ parameterName }: ${error}`, },
+      { status: 400, },
+    )
+  }
+}
