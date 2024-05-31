2b75375f5d463031ca6c0a3dda36fc31522cecd1
platocrat <jack.abe.winfield@gmail.com>
2024-05-30 21:46:23 -0500

    clean: React context providers to layout components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,22 +7,17 @@ import { usePathname, useRouter } from 'next/navigation'
 import Header from '@/components/Header'
 import Spinner from '@/components/Suspense/Spinner'
 // Contexts
-import { UserDemographicContext } from '@/contexts/UserDemographicContext'
+import UserDemographicsLayout from '@/components/Layouts/UserDemographics'
 import { BessiSkillScoresContext } from '@/contexts/BessiSkillScoresContext'
 import { AuthenticatedUserContext } from '@/contexts/AuthenticatedUserContext'
 import { CurrentParticipantStudyContext } from '@/contexts/CurrentParticipantStudyContext'
-// Utils
-import {
-  YesOrNo,
-  USState,
-  SocialClass,
-  RaceOrEthnicity,
-} from '@/utils'
 // Types
 import { BessiSkillScoresType, STUDY_SIMPLE__DYNAMODB } from '@/utils'
 // CSS
 import './globals.css'
 import { definitelyCenteredStyle } from '@/theme/styles'
+import BessiSkillScoresLayout from '@/components/Layouts/BessiSkillScoresLayout'
+import CurrentParticipantStudyLayout from '@/components/Layouts/CurrentParticipantStudyLayout'
 
 
 
@@ -60,181 +55,17 @@ export default function RootLayout({
   const router = useRouter()
   const pathname = usePathname()
 
-  // Custom state variables
-  const [ 
-    bessiSkillScores, 
-    setBessiSkillScores 
-  ] = useState<BessiSkillScoresType | null>(null)
   // State variables for `UserType`
   const [ email, setEmail ] = useState<string>('')
   const [ username, setUsername ] = useState<string>('')
   const [ isAdmin, setIsAdmin ] = useState<boolean>(false)
   const [ isParticipant, setIsParticipant ] = useState<boolean>(false)
   const [ userStudies, setUserStudies ] = useState<STUDY_SIMPLE__DYNAMODB[]>([])
-  // State variables to select the participant's current study
-  const [ 
-    currentStudy, 
-    setCurrentStudy 
-  ] = useState<STUDY_SIMPLE__DYNAMODB | null>(null)
   // Booleans for user authentication
   const [ isInviteUrl, setIsInviteUrl] = useState<boolean>(false)
   const [ isFetchingUser, setIsFetchingUser ] = useState<boolean>(true)
   const [ isAuthenticated, setIsAuthenticated ] = useState<boolean>(false)
-  // State variables UserDemographicsContext
-  // Numbers
-  const [ age, setAge ] = useState<number>(0)
-  const [ familySize, setFamilySize ] = useState<number>(0)
-  const [
-    annualHouseholdIncome, 
-    setAnnualHouseholdIncome 
-  ] = useState<number>(0)
-  // Regular strings
-  const [
-    areaOfScienceTraining,
-    setAreaOfScienceTraining,
-  ] = useState<string>('')
-  const [ zipCode, setZipCode ] = useState<string>('')
-  const [ foreignCountry, setForeignCountry ] = useState<string>('')
-  // Enums
-  const [ isParent, setIsParent ] = useState<YesOrNo>(YesOrNo.No)
-  const [ usState, setUSState ] = useState<USState>(USState.Alabama)
-  const [ priorCompletion, setPriorCompletion ] = useState<YesOrNo>(YesOrNo.No)
-  const [
-    isFluentInEnglish,
-    setIsFluentInEnglish
-  ] = useState<YesOrNo>(YesOrNo.No)
-  const [
-    raceOrEthnicity,
-    setRaceOrEthnicity
-  ] = useState<RaceOrEthnicity[]>([ ])
-  // Generics
-  const [
-    highestFormalEducation,
-    setHighestFormalEducation
-  ] = useState<string | number>('')
-  const [
-    currentEmploymentStatus,
-    setCurrentEmploymentStatus
-  ] = useState<string | number>('')
-  const [
-    currentMaritalStatus,
-    setCurrentMaritalStatus
-  ] = useState<string | number>('')
-  const [
-    socialClass,
-    setSocialClass
-  ] = useState<SocialClass>(SocialClass.LowerMiddleClass)
-  const [ gender, setGender ] = useState<string | number>('')
-  const [ religion, setReligion ] = useState<string | number>('')
 
-
-
-  // -------------------- Form Input handler functions -------------------------
-  function onPriorCompletionChange(e: any) {
-    const value = e.target.value
-    // console.log(`e.target.value: `, e.target.value)
-    setPriorCompletion(value)
-  }
-
-  function onGenderChange(e: any) {
-    const _ = e.target.value
-    // console.log(`gender: `, _)
-    setGender(_)
-  }
-
-  function onAgeChange(e: any) {
-    const _ = e.target.value
-    // console.log(`age: `, _)
-    setAge(_)
-  }
-
-  function onRaceOrEthnicityChange(e: any) {
-    const _ = Object.values(RaceOrEthnicity)[e.target.value]
-    
-    setRaceOrEthnicity((previousState) => {
-      if (previousState.includes(_)) {
-        // If the current state is already in the array, remove it
-        return previousState.filter(item => item !== _)
-      } else {
-        // Otherwise, add it to the array
-        return [...previousState, _]
-      }
-    })
-  }
-
-  function onEnglishFluencyChange(e: any) {
-    // console.log(`e.target.value: `, e.target.value)
-    const _ = e.target.value
-    setIsFluentInEnglish(_)
-  }
-
-  function onSocialClassChange(e: any) {
-    // console.log(`e.target.value: `, e.target.value)
-    const _ = e.target.value
-    setSocialClass(_)
-  }
-
-  function onUsLocationChange(e: any) {
-    // console.log(`e.target.value: `, e.target.value)
-    const _ = e.target.value
-    setUSState(_)
-  }
-
-  function onZipCodeChange(e: any) {
-    // console.log(`e.target.value: `, e.target.value)
-    const _ = e.target.value
-    setZipCode(_)
-  }
-
-  function onForeignLocationChange(e: any) {
-    // console.log(`e.target.value: `, e.target.value)
-    const _ = e.target.value
-    setForeignCountry(_)
-  }
-
-  function onHighestEducationLevelChange(e: any) {
-    // console.log(`e.target.value: `, e.target.value)
-    const _ = e.target.value
-    setHighestFormalEducation(_)
-  }
-
-  function onCurrentEmploymentStatusChange(e: any) {
-    // console.log(`e.target.value: `, e.target.value)
-    const _ = e.target.value
-    setCurrentEmploymentStatus(_)
-  }
-
-  function onCurrentMaritalStatusChange(e: any) {
-    // console.log(`e.target.value: `, e.target.value)
-    const _ = e.target.value
-    setCurrentMaritalStatus(_)
-  }
-
-  function onIsParentChange(e: any) {
-    // console.log(`e.target.value: `, e.target.value)
-    const _ = e.target.value
-    setIsParent(_)
-  } 
-
-  function onFamilySizeChange(e: any) {
-    const _ = e.target.value
-    setFamilySize(_)
-  }
-
-  function onReligionChange(e: any) {
-    const _ = e.target.value
-    setReligion(_)
-  }
-  
-  function onAnnualHouseholdIncomeChange(e: any) {
-    const _ = e.target.value
-    setAnnualHouseholdIncome(_)
-  }
-  
-  function onAreaOfScienceTrainingChange(e: any) {
-    const _ = e.target.value
-    setAreaOfScienceTraining(_)
-  }
 
 
   // --------------------------- Async functions -------------------------------
@@ -388,63 +219,14 @@ export default function RootLayout({
                   setIsAuthenticated,
                 }}
               >
-                <CurrentParticipantStudyContext.Provider
-                  value={{
-                    currentStudy,
-                    setCurrentStudy,
-                  }}
-                >
-                  <UserDemographicContext.Provider
-                    value={{
-                      // State variables
-                      age,
-                      gender,
-                      usState,
-                      zipCode,
-                      religion,
-                      isParent,
-                      familySize,
-                      socialClass,
-                      foreignCountry,
-                      raceOrEthnicity,
-                      priorCompletion,
-                      isFluentInEnglish,
-                      currentMaritalStatus,
-                      areaOfScienceTraining,
-                      annualHouseholdIncome,
-                      highestFormalEducation,
-                      currentEmploymentStatus,
-                      // Form input handlers
-                      onAgeChange,
-                      onGenderChange,
-                      onZipCodeChange,
-                      onReligionChange,
-                      onIsParentChange,
-                      onUsLocationChange,
-                      onFamilySizeChange,
-                      onSocialClassChange,
-                      onEnglishFluencyChange,
-                      onRaceOrEthnicityChange,
-                      onPriorCompletionChange,
-                      onForeignLocationChange,
-                      onCurrentMaritalStatusChange,
-                      onAreaOfScienceTrainingChange,
-                      onHighestEducationLevelChange,
-                      onAnnualHouseholdIncomeChange,
-                      onCurrentEmploymentStatusChange,
-                    }}
-                  >
-                    <BessiSkillScoresContext.Provider
-                      value={{
-                        bessiSkillScores,
-                        setBessiSkillScores,
-                      }}
-                    >
+                <CurrentParticipantStudyLayout>
+                  <UserDemographicsLayout>
+                    <BessiSkillScoresLayout>
                       { isAuthenticated && <Header/> }
                       { children }
-                    </BessiSkillScoresContext.Provider>
-                  </UserDemographicContext.Provider>
-                </CurrentParticipantStudyContext.Provider>
+                    </BessiSkillScoresLayout>
+                  </UserDemographicsLayout>
+                </CurrentParticipantStudyLayout>
               </AuthenticatedUserContext.Provider>
             </body>
           </html>
---
+++ src/components/Layouts/BessiSkillScoresLayout/index.tsx
@@ -0,0 +1,37 @@
+// Externals
+import { ReactNode, FC, useState } from 'react'
+// Locals
+import { BessiSkillScoresType } from '@/utils'
+import { BessiSkillScoresContext } from '@/contexts/BessiSkillScoresContext'
+
+
+type BessiSkillScoresLayoutProps = {
+  children: ReactNode
+}
+
+
+
+
+const BessiSkillScoresLayout: FC<BessiSkillScoresLayoutProps> = ({ children }) => {
+  const [
+    bessiSkillScores,
+    setBessiSkillScores
+  ] = useState<BessiSkillScoresType | null>(null)
+
+
+  return (
+    <>
+      <BessiSkillScoresContext.Provider
+        value={ {
+          bessiSkillScores,
+          setBessiSkillScores,
+        } }
+      >
+        { children }
+      </BessiSkillScoresContext.Provider>
+    </>
+  )
+}
+
+
+export default BessiSkillScoresLayout
