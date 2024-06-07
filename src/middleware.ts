cb160eca7ddd00ef3d337b71ca3a787573c48f65
platocrat <jack.abe.winfield@gmail.com>
2024-06-07 01:33:45 -0500

    fix: /api/auth/logout for Auth0; deprecate old API endpoints to v1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,4 @@
 // middleware.js
-import { NextResponse } from 'next/server'
-import { withMiddlewareAuthRequired, getSession } from '@auth0/nextjs-auth0/edge'
-import { COOKIE_NAME } from './utils'
+import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge'
 
-
-export default withMiddlewareAuthRequired(async function middleware(req) {
-  const res = NextResponse.next()
-  const user = await getSession(req, res)
-
-  console.log(`user?.user: `, user?.user)
-  
-  res.cookies.set(COOKIE_NAME, JSON.stringify(user?.user))
-  
-  return res
-})
+export default withMiddlewareAuthRequired()
