46881633a9513f4572ec03de71c0c59923b674af
platocrat <jack.abe.winfield@gmail.com>
2024-07-05 16:32:15 -0500

    improve page to generate fictional pop-culture characters

--- src/sections/assessments/bessi/assessment/results/skills-scores-and-definitions/index.tsx
+++ src/sections/assessments/bessi/assessment/results/skills-scores-and-definitions/index.tsx
@@ -6,12 +6,32 @@ import { FC, Fragment, useContext } from 'react'
 // Contexts
 import { BessiSkillScoresContext } from '@/contexts/BessiSkillScoresContext'
 // Constants
-import { FacetFactorType, SkillDomainFactorType, SKILLS_MAPPING } from '@/utils'
+import { 
+  getRangeLabel, 
+  SKILLS_MAPPING, 
+  FacetFactorType, 
+  SkillDomainFactorType, 
+} from '@/utils'
 // CSS
 import styles from '@/app/page.module.css'
 
 
 
+const getColorForScore = (score) => {
+  if (score >= 0 && score <= 20) {
+    return '#ff0000' // Red for very low
+  } else if (score > 20 && score <= 40) {
+    return '#ff8000' // Orange for low
+  } else if (score > 40 && score <= 60) {
+    return '#ffff00' // Yellow for medium
+  } else if (score > 60 && score <= 80) {
+    return '#80ff00' // Light green for high
+  } else if (score > 80 && score <= 100) {
+    return '#00ff00' // Green for very high
+  } else {
+    return '#000000' // Black for out of range (error case)
+  }
+}
 
 
 const SkillsScoresAndDefinitionsTable: FC<{
@@ -21,174 +41,148 @@ const SkillsScoresAndDefinitionsTable: FC<{
   facetScores,
   domainScores,
 }) => {
-  const { bessiSkillScores } = useContext(BessiSkillScoresContext)
+    const { bessiSkillScores } = useContext(BessiSkillScoresContext)
 
 
-  return (
-    <>
-      <table className={ styles.bessi_assessment_table }>
-        <tbody>
-          <tr>
-            { SKILLS_MAPPING.tableHeaders.map(( header: string, i: number) => (
-              <Fragment 
-                key={ `skills-scores-and-definitions-table-headers-${ i }` }
-              >
-                <th
-                  style={{ 
-                    textAlign: 'center',
-                    width: i === SKILLS_MAPPING.tableHeaders.length - 1 
-                      ? '70%' 
-                      : '15%',
-                  }} 
-                >
-                  <div>
-                    <strong>
-                      <span className={ styles.bessi_text1 }>
-                        { header }
-                        <br />
-                        { `(0-100)` }
-                      </span>
-                    </strong>
-                  </div>
-                </th>
-              </Fragment>
-            )) }
-          </tr>
-          
-          { Object.keys(SKILLS_MAPPING.domains).map(( 
-            domain: string, 
-            i: number 
-          ) => (
-            <Fragment key={ `skills-scores-and-definitions-${ domain }-${ i }` }>
-              <tr>
-                <td style={{ textAlign: 'center' }}>
-                  <span className={ styles.bessi_header2 }>
-                    { i === Object.keys(SKILLS_MAPPING.domains).length - 1 
-                      ? null
-                      : (
-                        <>
-                          <p style={{ fontSize: 'clamp(14px, 2.5vw, 18px)' }}>
-                            { domain }
-                          </p>
-                          <br />
-                          <p style={{ fontSize: 'clamp(14px, 2.5vw, 18px)' }}>
-                            { 
-                              domainScores 
-                                ? domainScores[domain] ?? 0
-                                : bessiSkillScores?.domainScores[domain] ?? 0 
-                            }
-                          </p>
-                        </>
-                    )}
+
+
+    return (
+      <div>
+        { Object.keys(SKILLS_MAPPING.domains).map((domain: string, i: number) => (
+          <div 
+            key={ `skills-scores-and-definitions-${domain}-${i}` } 
+            className={ styles.card }
+            style={{
+              padding: '16px',
+              marginBottom: '16px',
+            }}
+          >
+            <div className={ styles.card_body }>
+              <div className={ styles.card_item }>
+                <div className={ styles.bessi_domain_header }>
+                  { domain }
+                  <span
+                    className={ styles.bessi_definition_score }
+                    style={ {
+                      position: 'relative',
+                      top: '-4px',
+                      fontSize: 'clamp(14px, 2.5vw, 18px)'
+                    } }
+                  >
+                    { domainScores 
+                      ? domainScores[domain] ?? 0 
+                      : bessiSkillScores?.domainScores[domain] ?? 0 
+                    }
                   </span>
-                </td>
-
-                <td
-                  style={ { textAlign: 'center' } }
-                  className={ styles.bessi_assessment_td }
-                >
-                  { Object.keys(SKILLS_MAPPING.domains[domain].facets).map((
-                    facet: string,
-                    j: number
-                  ) => (
-                    <Fragment 
-                      key={ 
-                        `skills-scores-and-definitions-${ domain }-${ facet }-${i}`
-                      }
+                  {/* Score range label */}
+                  <div 
+                    style={{ 
+                      backgroundColor: getColorForScore(
+                          domainScores
+                            ? domainScores[domain] ?? 0
+                            : bessiSkillScores?.domainScores[domain] ?? 0
+                        ), 
+                      borderRadius: '5px', 
+                      padding: '0px 7.5px',
+                      marginLeft: '14px',
+                      position: 'relative',
+                      top: '-1px',
+                    }}
+                  >
+                    <p 
+                      style={{ 
+                        color: 'white', 
+                        fontWeight: '800', 
+                        filter: 'drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.85))',
+                        position: 'relative',
+                        top: '4.5px',
+                      }}
                     >
-                      <div
-                        style={{
-                          lineHeight: '18px',
-                          marginBottom: '24px',
+                      { 
+                        getRangeLabel(
+                          domainScores 
+                            ? domainScores[domain] ?? 0 
+                            : bessiSkillScores?.domainScores[domain] ?? 0
+                        ) 
+                      }
+                    </p>
+                  </div>
+                </div>
+                <div className={ styles.bessi_definition }>
+                  <span className={ styles.bessi_definition_text }>
+                    { SKILLS_MAPPING.domains[domain].description }
+                  </span>
+                </div>
+              </div>
+
+              <div className={ styles.facets_grid }>
+                { Object.keys(SKILLS_MAPPING.domains[domain].facets).map((facet: string, j: number) => (
+                  <div key={ `skills-scores-and-definitions-${domain}-${facet}-${j}` } className={ styles.card_item }>
+                    <div className={ styles.bessi_definition }>
+                      <strong 
+                        style={{ 
+                          display: 'flex', 
+                          flexDirection: 'row' 
                         }}
                       >
-                        <span className={ styles.bessi_text1 }>
-                          <strong 
-                            style={{ fontSize: 'clamp(11px, 2.5vw, 14px)' }}
-                          >
-                            { facet }
-                            <br />
-                            { 
+                        { facet }
+
+                        <span 
+                          className={ styles.bessi_definition_score }
+                          style={{
+                            position: 'relative',
+                            top: '-3px',
+                          }}
+                        >
+                          { facetScores ? facetScores[facet] ?? 0 : bessiSkillScores?.facetScores[facet] ?? 0 }
+                        </span>
+                        
+                        {/* Score range label */ }
+                        <div
+                          style={ {
+                            backgroundColor: getColorForScore(
                               facetScores
                                 ? facetScores[facet] ?? 0
-                                : bessiSkillScores?.facetScores[facet] ?? 0  
-                            }
-                          </strong>
-                        </span>
-                      </div>
-                    </Fragment>
-                  )) }
-                </td>
-
-                <td
-                  valign="top"
-                  className={ styles.bessi_assessment_td }
-                  style={{ textAlign: 'left' }}
-                >
-                  <div>
-                    <span className={ styles.bessi_text1 }>
-                      <strong 
-                        style={{ fontSize: 'clamp(12px, 2.5vw, 16px)' }}
-                      >
-                        { domain  }
-                      </strong>
-                      <br />
-                      <div 
-                        style={{ 
-                          marginBottom: '4px', 
-                          lineHeight: '1.2',
-                          fontSize: 'clamp(10px, 2.5vw, 14px)'
-                        }}
-                      >
-                        { SKILLS_MAPPING.domains[domain].description }
-                      </div>
-                    </span>
-                  </div>
-
-                  { Object.keys(SKILLS_MAPPING.domains[domain].facets).map((
-                    facet: string,
-                    k: number
-                  ) => (
-                    <Fragment 
-                      key={ 
-                        `skills-scores-and-definitions-${ domain }-${ facet }-${ k }`
-                      }
-                    >
-                      <div
-                        style={ {
-                          margin: '8px 0px 24px 0px'
-                        } }
-                      >
-                        <span className={ styles.bessi_text1 }>
-                          <em 
-                            style={{ 
-                              marginRight: '8px',
-                              fontSize: 'clamp(11px, 2.5vw, 14px)'
-                            }}
-                          >
-                            { `${ facet }:` }
-                          </em>
+                                : bessiSkillScores?.facetScores[facet] ?? 0
+                            ),
+                            borderRadius: '5px',
+                            padding: '0px 7.5px',
+                            marginLeft: '14px',
+                            position: 'relative',
+                          } }
+                        >
                           <p
-                            style={ { fontSize: 'clamp(11px, 2.5vw, 14px)' }}
+                            style={ {
+                              color: 'white',
+                              fontWeight: '800',
+                              filter: 'drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.85))',
+                              position: 'relative',
+                              top: '2.5px',
+                            } }
                           >
-                            { SKILLS_MAPPING.domains[domain].facets[facet] }
+                            {
+                              getRangeLabel(
+                                facetScores
+                                  ? facetScores[facet] ?? 0
+                                  : bessiSkillScores?.facetScores[facet] ?? 0
+                              )
+                            }
                           </p>
-                        </span>
-                      </div>
-                    </Fragment>
-                  )) }
-                </td>
-              </tr>    
-            </Fragment>
-          )) }
-
-        </tbody>
-      </table>
-    </>
-  )
-}
-
-
+                        </div>
+                      </strong>
+                      <span className={ styles.bessi_definition_text }>
+                        { SKILLS_MAPPING.domains[domain].facets[facet] }
+                      </span>
+                    </div>
+                  </div>
+                )) }
+              </div>
+            </div>
+          </div>
+        )) }
+      </div>
+    )
+  }
 
 const BessiResultsSkillsScoresAndDefinitions: FC<{
   facetScores?: FacetFactorType,
@@ -197,22 +191,20 @@ const BessiResultsSkillsScoresAndDefinitions: FC<{
   facetScores,
   domainScores,
 }) => {
-  const title = `Skill Scores and Definitions`
+    const title = `Skill Scores and Definitions`
 
-  return (
-    <>
-      <div style={{ margin: '48px 0px 0px 0px' }}>
-        <div style={{ margin: '18px 0px 18px 0px' }}>
-          <h3 style={{ fontSize: 'clamp(16px, 2.5vw, 16px)' }}>{ title }</h3>
+    return (
+      <div style={ { margin: '12px 0px 48px 0px' } }>
+        <div style={ { margin: '18px 0' } }>
+          <h3 style={ { fontSize: 'clamp(16px, 2.5vw, 16px)' } }>{ title }</h3>
         </div>
 
-        <SkillsScoresAndDefinitionsTable 
+        <SkillsScoresAndDefinitionsTable
           facetScores={ facetScores }
           domainScores={ domainScores }
         />
       </div>
-    </>
-  )
-}
+    )
+  }
 
 export default BessiResultsSkillsScoresAndDefinitions
--- src/sections/social-rating/fictional-characters/index.tsx
+++ src/sections/social-rating/fictional-characters/index.tsx
@@ -1,7 +1,15 @@
 'use client'
 
 // Externals
-import React, { Fragment, useContext, useState, useEffect } from 'react'
+import { 
+  Fragment, 
+  Dispatch,
+  useState, 
+  useEffect, 
+  useContext, 
+  SetStateAction,
+  FC, 
+} from 'react'
 // Locals
 // Components
 import Spinner from '@/components/Suspense/Spinner'
@@ -23,9 +31,19 @@ import {
 } from '@/utils'
 // CSS
 import { definitelyCenteredStyle } from '@/theme/styles'
+import mainPortalStyle from '@/sections/main-portal/MainPortal.module.css'
 import styles from '@/sections/social-rating/fictional-characters/FictionalCharacters.module.css'
 
-type Character = {
+
+
+
+type FictionalCharactersProps = {
+  
+}
+
+
+
+export type CharacterType = {
   name: string
   group: string
   description: string
@@ -33,9 +51,11 @@ type Character = {
   domainScores: SkillDomainFactorType
 }
 
+
+
 function updateCharacters(
-  characters,
-  setCharacters,
+  characters: CharacterType[],
+  setCharacters: Dispatch<SetStateAction<CharacterType[]>>,
   genCharacters: {
     group: string
     name: string
@@ -46,7 +66,7 @@ function updateCharacters(
       activity: string
     }[]
   }[],
-  setProgress
+  setProgress: Dispatch<SetStateAction<number>>
 ) {
   function update(
     genCharacter: any,
@@ -74,7 +94,7 @@ function updateCharacters(
 
     const { facetScores, domainScores } = calculateBessiScores(responses)
 
-    const character: Character = {
+    const character: CharacterType = {
       group,
       name,
       description,
@@ -110,10 +130,14 @@ function updateCharacters(
 // OpenAI API request function
 const generateCharacterProfile = async (
   prompt: string,
-  characters,
-  setCharacters,
-  setProgress
+  characters: CharacterType[],
+  setCharacters: Dispatch<SetStateAction<CharacterType[]>>,
+  setTotalCharacters: Dispatch<SetStateAction<number>>,
+  setProgress: Dispatch<SetStateAction<number>>,
+  setCurrentPromptIndex: Dispatch<SetStateAction<number>>,
 ): Promise<void> => {
+  setCurrentPromptIndex(prevIndex => prevIndex + 1)
+
   const SYSTEM_CONTENT = `You will be provided with a fictional pop-culture series name (e.g. Harry Potter, Game of Thrones, Euphoria, The Big Bang Theory, American Horror Story, etc.). Your task is to simulate responses to the following 45 activities for up to 3 characters that are from the given pop-culture series: for each activity, respond with a rating between 1 and 5 to represent how others would rate that fictional pop-culture character, with 1 representing not at all likely to do the activity and 5 representing very much likely to do the activity. You must give the rating for each of the 45 activities in your response. Additionally, you must give the description of the personality of the character. For example, if given 'Harry Potter', you should respond with 'Harry Potter from the Harry Potter series is the brave protagonist of the series. Now, here are the responses to each of the 45 activities:'. The description must be around 250 words. Remember that this must be for each character. Furthermore, return your response as an array of JSON objects where each JSON object includes the description, the list of 45-ratings, and the character's name. To be clear, the type of the response that you must return is, 'type ResponsesType = { group: string, name: string, description: string, responses: { response: number, activity: string, id: number }[] }[]'. Below is the list of activities: ${BESSI_45_ACTIVITIES.join('?')}`
 
   const apiEndpoint = 'https://api.openai.com/v1/chat/completions'
@@ -162,9 +186,20 @@ const generateCharacterProfile = async (
   } else {
     try {
       let genCharacters = JSON.parse(content)
-      console.log(`JSON.parse(content): `, genCharacters)
-      genCharacters = genCharacters.length > 0 ? genCharacters[0] : genCharacters
-      updateCharacters(characters, setCharacters, genCharacters, setProgress)
+
+      const _totalCharacters = genCharacters.length
+      
+      setTotalCharacters(
+        prevTotalCharacters => prevTotalCharacters + _totalCharacters
+      )
+      
+      if (_totalCharacters > 0) {
+        genCharacters.forEach((_) => (
+          updateCharacters(characters, setCharacters, _, setProgress)
+        ))
+      } else {
+        updateCharacters(characters, setCharacters, genCharacters, setProgress)
+      }
     } catch (error) {
       console.error(`End delimiter not found. Here is the content: `, content)
       console.error(`End delimiter not found. Here is the error: `, error)
@@ -175,28 +210,36 @@ const generateCharacterProfile = async (
 
 
 
-const FictionalCharacters: React.FC = () => {
+
+
+const FictionalCharacters: FC<FictionalCharactersProps> = ({
+
+}) => {
   // States
-  const [loading, setLoading] = useState<boolean>(false)
-  const [completed, setCompleted] = useState<boolean>(false)
-  const [characters, setCharacters] = useState<Character[]>([])
-  const [progress, setProgress] = useState<number>(0)
+  const [ characters, setCharacters ] = useState<CharacterType[]>([])
+  // Booleans
+  const [ loading, setLoading ] = useState<boolean>(false)
+  const [ completed, setCompleted ] = useState<boolean>(false)
+  // Numbers
+  const [ progress, setProgress ] = useState<number>(0)
+  const [ totalCharacters, setTotalCharacters ] = useState<number>(0)
+  const [ currentPromptIndex, setCurrentPromptIndex ] = useState<number>(0)
 
   const prompts = [
-    'Generate a personality profile for a character from American Horror Story.',
-    'Generate a personality profile for a character from Euphoria.',
-    'Generate a personality profile for a character from The Big Bang Theory.',
+    'Generate a personality profile for a single character from American Horror Story.',
+    'Generate a personality profile for a single character from Euphoria.',
+    'Generate a personality profile for a single character from The Big Bang Theory.',
     // Add more prompts if needed
   ]
 
-  const totalCharacters = prompts.length
+  const totalPrompts = prompts.length
 
   const generateCharacters = async () => {
     setLoading(true)
     setCompleted(false)
     setProgress(0)
 
-    const newCharacters: Character[] = []
+    const newCharacters: CharacterType[] = []
     setCharacters(newCharacters) // Remove any previously generated characters
 
     for (const prompt of prompts) {
@@ -204,7 +247,9 @@ const FictionalCharacters: React.FC = () => {
         prompt,
         characters,
         setCharacters,
-        setProgress
+        setTotalCharacters,
+        setProgress,
+        setCurrentPromptIndex,
       )
     }
 
@@ -213,7 +258,7 @@ const FictionalCharacters: React.FC = () => {
 
     setTimeout(() => {
       setCompleted(false)
-    }, 3_000) // Hide the notification after 3 seconds
+    }, 8_000) // Hide the notification after 8 seconds
   }
 
 
@@ -252,7 +297,8 @@ const FictionalCharacters: React.FC = () => {
             : `Generate Characters`
           }
         </button>
-        {/* Loading indicator and progress completion */ }
+
+        { /* Loading indicator and progress completion */ }
         { loading && (
           <>
             <div className={ styles['loading-container'] }>
@@ -265,68 +311,107 @@ const FictionalCharacters: React.FC = () => {
             </div>
           </>
         ) }
-        {/* Progress completion */ }
-        { loading && (
-          <div className={ styles['progress-container'] }>
-            { `Generated ${progress} of ${totalCharacters} characters` }
-            <div className={ styles['progress-bar'] }>
-              <div
-                className={ styles['progress-bar-fill'] }
-                style={ { width: `${(progress / totalCharacters) * 100}%` } }
-              ></div>
-            </div>
-          </div>
-        ) }
-        {/* Green success notification */ }
-        { completed && (
-          <>
+
+        { /* Progress completion */ }
+        <div className={ styles['progress-container'] }>
+          { `Generated ${progress} characters from ${
+              loading 
+                ? currentPromptIndex - 1
+                : currentPromptIndex
+              }/${totalPrompts} prompts. ${
+            currentPromptIndex !== totalPrompts ? `On prompt ${ currentPromptIndex }` : ''
+          }` }
+          <div className={ styles['progress-bar'] }>
             <div
-              className={
-                `${styles['notification-card']} ${completed ? '' : styles['hide']}`
-              }
-            >
-              { `Character generation complete!` }
-            </div>
-          </>
-        ) }
+              className={ styles['progress-bar-fill'] }
+              style={ { width: `${( currentPromptIndex - 1 / totalPrompts) * 100}%` } }
+            />
+          </div>
+        </div>
+
+        { /* Green success notification */ }
+        <div
+          style={ {
+            ...definitelyCenteredStyle,
+            position: 'absolute',
+            zIndex: '1000',
+            top: '0px',
+            right: '8px',
+          } }
+        >
+          { completed && (
+            <>
+              <div
+                style={{ 
+                  display: 'flex', 
+                  flexDirection: 'row', 
+                  padding: '12px 18px' 
+                }}
+                className={
+                  `${styles['notification-card']} ${completed ? '' : styles['hide']}`
+                }
+              >
+                { `Generated ${ totalCharacters } characters from ${totalPrompts} prompts.` }
+                <button
+                  onClick={ () => setCompleted(false) }
+                  className={ mainPortalStyle['close-button'] }
+                  style={ {
+                    position: 'relative',
+                    top: '-1px',
+                    right: '3px',
+                    fontSize: '16px',
+                    color: '#155724',
+                    background: 'none',
+                    border: 'none',
+                    cursor: 'pointer',
+                    marginLeft: '12px',
+                    padding: '0px',
+                  } }
+                >
+                  &times;
+                </button>
+              </div>
+            </>
+          ) }
+        </div>
         
-        {/* AI-generated Characters */ }
+        { /* AI-generated Characters */ }
         { characters.map((character, index) => (
           <Fragment key={ index }>
             <div key={ index } className={ styles['character-card'] }>
-            <div
-              style={ {
-                display: 'flex',
-                flexDirection: 'column',
-                marginBottom: '12px',
-              } }
-            >
-              <h3>
-                { `${character.name}` }
-              </h3>
-              <em>
-                <h4>
-                  { `${character.group}` }
-                </h4>
-              </em>
+              <div
+                style={ {
+                  display: 'flex',
+                  flexDirection: 'column',
+                  marginBottom: '12px',
+                } }
+              >
+                <h3>
+                  { `${character.name} (${index + 1}/${characters.length})` }
+                </h3>
+                <em>
+                  <h4>
+                    { `${character.group}` }
+                  </h4>
+                </em>
+              </div>
+
+              <p style={ { fontSize: 'clamp(11px, 2.5vw, 14px)' } }>
+                { character.description }
+              </p>
+
+              <BessiResultsVisualization
+                isExample={ true }
+                facetScores={ character.facetScores }
+                domainScores={ character.domainScores }
+              />
+
+              <BessiResultsSkillsScoresAndDefinitions
+                facetScores={ character.facetScores }
+                domainScores={ character.domainScores }
+              />
             </div>
-
-            <p style={ { fontSize: 'clamp(11px, 2.5vw, 14px)' } }>
-              { character.description }
-            </p>
-
-            <BessiResultsVisualization
-              isExample={ true }
-              facetScores={ character.facetScores }
-              domainScores={ character.domainScores }
-            />
-
-            <BessiResultsSkillsScoresAndDefinitions
-              facetScores={ character.facetScores }
-              domainScores={ character.domainScores }
-            />
-          </div>
-      </Fragment>
+          </Fragment>
         )) }
       </div>
     </>
