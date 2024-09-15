476fdebe2a873c2a72896cf6c32318541a7dd461
platocrat <jack.abe.winfield@gmail.com>
2024-09-14 19:41:34 -0500

    fix: React Context Layout for BessiSkillsScores

--- src/components/Layouts/BessiSkillScoresLayout/index.tsx
+++ src/components/Layouts/BessiSkillScoresLayout/index.tsx
@@ -12,34 +12,13 @@ type BessiSkillScoresLayoutProps = {
 
 
 
-const INIT_BESSI_SKILL_SCORES = {
-  id: '',
-  studyId: '',
-  accessToken: '',
-  facetScores: Object.values(Facet).reduce(
-    (acc, facet) => {
-      acc[facet] = 0
-      return acc
-    }, {} as { [key in Facet]: number }
-  ), // Initialize all facet scores with 0
-  domainScores: Object.values(SkillDomain).reduce(
-    (acc, domain) => {
-      acc[domain] = 0
-      return acc
-    }, {} as { [key in SkillDomain]: number }
-  ), // Initialize all domain scores with 0
-}
-
-
-
-
 const BessiSkillScoresLayout: FC<BessiSkillScoresLayoutProps> = ({ 
   children,
 }) => {
   const [
     bessiSkillScores,
     setBessiSkillScores
-  ] = useState<BessiSkillScoresType>(INIT_BESSI_SKILL_SCORES)
+  ] = useState<BessiSkillScoresType | null>(null)
 
 
   return (
