d39002da8fee58feb3d27aad439ab24d717f9fce
platocrat <jack.abe.winfield@gmail.com>
2024-09-14 19:36:20 -0500

    fix: type error on BessiSkillScoresContext

--- src/contexts/BessiSkillScoresContext.ts
+++ src/contexts/BessiSkillScoresContext.ts
@@ -8,28 +8,8 @@ import { BessiSkillScoresType, Facet, SkillDomain } from '@/utils'
 
 
 
-const INIT_BESSI_SKILL_SCORES: BessiSkillScoresType = {
-  id: '',
-  studyId: '',
-  accessToken: '',
-  facetScores: Object.values(Facet).reduce(
-    (acc, facet) => {
-      acc[facet] = 0
-      return acc
-    }, {} as { [key in Facet]: number }
-  ), // Initialize all facet scores with 0
-  domainScores: Object.values(SkillDomain).reduce(
-    (acc, domain) => {
-      acc[domain] = 0
-      return acc
-    }, {} as { [key in SkillDomain]: number }
-  ), // Initialize all domain scores with 0
-}
-
-
-
 const INIT_BESSI_SCORES_CONTEXT: BessiSkillScoresContextType = {
-  bessiSkillScores: INIT_BESSI_SKILL_SCORES,
+  bessiSkillScores: null,
   setBessiSkillScores: () => {},
 }
 
