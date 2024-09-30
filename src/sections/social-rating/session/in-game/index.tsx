fafbe03f7fa643ac3516a2d97291079bd2d13204
platocrat <jack.abe.winfield@gmail.com>
2024-09-30 11:11:09 -0500

    fix: use TLS secure WebSocket connection URL

--- src/sections/social-rating/session/in-game/index.tsx
+++ src/sections/social-rating/session/in-game/index.tsx
@@ -46,7 +46,7 @@ const MAX_RECONNECT_ATTEMPTS = 5 // Set a maximum number of attempts
  * 4. `updatePlayer` - HTTP route
  */
 const WEB_SOCKET_URLS = {
-  local: 'ws://localhost:3001/',
+  local: 'wss://localhost:3001/',
   'http-only': 'wss://vpfscho95i.execute-api.us-east-1.amazonaws.com/production/'
 }
 
