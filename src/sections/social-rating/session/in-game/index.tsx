aca70989588f8016885807767616915f1a013e77
platocrat <jack.abe.winfield@gmail.com>
2024-09-30 21:38:36 -0500

    revert to new AWS WebSocket API URL

--- src/sections/social-rating/session/in-game/index.tsx
+++ src/sections/social-rating/session/in-game/index.tsx
@@ -47,9 +47,8 @@ const MAX_RECONNECT_ATTEMPTS = 5 // Set a maximum number of attempts
  */
 const WEB_SOCKET_URLS = {
   local: 'wss://localhost:3001/',
-  'http-only': 'wss://vpfscho95i.execute-api.us-east-1.amazonaws.com/production/',
-  'http-only-2': 'wss://wewbqdsubc.execute-api.us-east-1.amazonaws.com/production/',
-  // 'lambda-functions': 'wss://vpfscho95i.execute-api.us-east-1.amazonaws.com/production'
+  'http-only': 'wss://wewbqdsubc.execute-api.us-east-1.amazonaws.com/production/',
+  'lambda-functions': 'wss://vpfscho95i.execute-api.us-east-1.amazonaws.com/production/'
 }
 
 
